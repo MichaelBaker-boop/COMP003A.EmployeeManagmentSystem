4e1886e8dac60b587ccf70be78408b16de481c5d
Michael Baker <michaelbaker4@my.whccd.edu>
2025-02-12 10:57:26 -0800

    Code Management

---
+++ COMP003A.EmployeeManagmentSystem/HRDepartment.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.EmployeeManagmentSystem
+{
+    /// <summary>
+    /// HR Department class derived from Department class.
+    /// </summary>
+    class HRDepartment : Department, IDepartmentOperations
+    {
+        public string Name { get; set; }
+
+        public HRDepartment()
+        {
+
+            Name = "HR Department";
+
+        }
+
+        public override string GetDepartmentDetails()
+        {
+
+            return "Handles employee relations and recruitment.";
+
+        }
+
+        public void Operate()
+        {
+
+            Console.WriteLine("Performing HR Operations.");
+
+        }
+
+    }
+    
+    
+}
---
+++ COMP003A.EmployeeManagmentSystem/ITDepartment.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.EmployeeManagmentSystem
+{
+    /// <summary>
+    /// IT Department class derived from Department class.
+    /// </summary>
+    class ITDepartment : Department, IDepartmentOperations
+    {
+        public string Name { get; set; }
+
+        public ITDepartment()
+        {
+
+            Name = "IT Department";
+
+        }
+
+        public override string GetDepartmentDetails()
+        {
+
+            return "Manages technical resources and infrastructure.";
+
+        }
+
+        public void Operate()
+        {
+
+            Console.WriteLine("Performing IT Operations.");
+
+        }
+    }
+    
+    
+}
